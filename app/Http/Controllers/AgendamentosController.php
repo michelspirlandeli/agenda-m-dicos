3a2287fd14b828ca0cff31e7b532dec5af504f2e
michel spirlandeli <michelspirlandeli10@gmail.com>
2018-01-03 23:18:19 -0200

    Add controllers

---
+++ app/Http/Controllers/AgendamentosController.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests;
+use App\Agendamentos;
+use Validator;
+
+
+
+class AgendamentosController extends Controller
+{
+    protected function validarAgendamentos($request){
+        $validator = Validator::make($request->all(), [
+            "paciente" => "required",
+            "cpf"=> "required",
+            "medico" => "required",
+            "dataconsulta" => "required",
+            "horario" => "required",
+            "status" => "required"
+        ]);
+        return $validator;
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $agendamentos = Agendamentos::all();
+        return view('agendamentos.index', compact('agendamentos'));
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('agendamentos.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validator = $this->validarAgendamentos($request);
+        if($validator->fails()){
+            return redirect()->back()->withErrors($validator->errors());
+        }
+        $dados = $request->all();
+        Agendamentos::create($dados);
+
+        return redirect()->route('agendamentos.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $agendamentos = Agendamentos ::find($id);
+        return view('agendamentos.show', compact('agendamentos'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $agendamentos = Agendamentos ::find($id);
+        return view('agendamentos.edit', compact('agendamentos'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $validator = $this->validarAgendamentos($request);
+     
+        if($validator->fails()){
+        return redirect()->back()->withErrors($validator->errors());
+        }
+
+        $agendamentos = Agendamentos ::find($id);
+        $dados = $request->all();
+        $agendamentos->update($dados);
+        return redirect()->route('agendamentos.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/DoctorsController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Doctors;
+
+class DoctorsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $doctors = Doctors::all();
+        return view('doctors.index', compact('doctors'));
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('doctors.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $dados = $request->all();
+        Doctors::create($dados);
+
+        return redirect()->route('doctors.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
